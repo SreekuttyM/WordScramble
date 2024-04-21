517a10dfbe04937d236826043b0bfb012aca0c93
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-21 18:33:47 +0400

    List - set up

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            Section {
+                Text("Hello")
+                Text("Hello")
+            }
+            Section {
+                ForEach(0..<2) {_ in
+                    Text("Inside Hello")
+                }
+            }
+            Section {
+                Text("Hello")
+                Text("Hello")
+            }
+
+        }.listStyle(.insetGrouped)
+        
+        List(people, id:\.self) {
+            Text("Hello \($0)")
         }
-        .padding()
+         
+        
+        List {
+            Section {
+                Text("Hello")
+                Text("Hello")
+            }
+            Section {
+                ForEach(people,id:\.self) {
+                    Text("Hello \($0)")
+                }
+            }
+            Section {
+                Text("Hello")
+                Text("Hello")
+            }
+
+        }
+       
     }
 }
 

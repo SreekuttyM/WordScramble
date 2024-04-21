448bd76bef5b66179c72284eb69a1e2dfc43bc9f
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-21 18:11:13 +0400

    project setup added

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Sreekutty Maya on 21/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

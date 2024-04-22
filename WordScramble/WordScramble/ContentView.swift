97743dde6c3a17f80981c0b0a795b7b2a6e33877
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-22 15:37:18 +0400

    scrambled words quiz integration with swiftUi

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -11,27 +11,76 @@ struct ContentView: View {
    @State var usedWords : [String] = []
    @State var rootWord : String = ""
    @State var newWord : String = ""
+    @State var wordErroralertTitle : String = ""
+    @State var wordErrorDescirption : String = ""
+    @State var showAlert : Bool = false
 
     var body: some View {
         NavigationStack {
-            Section {
-                TextField("Enter a word here", text: $newWord).textInputAutocapitalization(.never)
-            }
-            
             List {
-                ForEach(usedWords,id:\.self) { word in
-                    HStack {
-                        Image(systemName: "\(word.count).circle")
-                        Text("\(word)")
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        Text(word)
                     }
                 }
+                
+                
+            }  .navigationTitle($rootWord)
+                .onSubmit({
+                    guard  isOriginalWord(word: newWord.lowercased()) else {
+                    wordError(title: "Word used already", message: "Be more original")
+                        return
+                    }
+                    guard isPossibleWord(word: newWord.lowercased()) else {
+                        wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+                        return
+                    }
+
+                    guard isReal(word:newWord.lowercased()) else {
+                        wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+                        return
+                    }
+                    addNewWord()
+                })
+               .onAppear(perform: startGame)
+        }.alert(wordErroralertTitle, isPresented: $showAlert) {}  message: {
+            Text(wordErrorDescirption)
+        }
+    }
+    
+    func isOriginalWord(word:String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isPossibleWord(word:String) -> Bool {
+        var tempWord = rootWord
+            for letter in word {
+                if let pos = tempWord.firstIndex(of: letter) {
+                    tempWord.remove(at: pos)
+                } else {
+                    return false
+                }
             }
-        }.navigationTitle($rootWord)
-            .onSubmit {
-                addNewWord()
-            }
-        
-       
+
+            return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        wordErroralertTitle = title
+        wordErrorDescirption = message
+        showAlert = true
     }
     
     func addNewWord() {
@@ -43,6 +92,20 @@ struct ContentView: View {
             }
         }
         newWord = ""
+    }
+    
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                
+                let words = startWords.split(separator: "\n")
+                rootWord = String(words.randomElement() ?? "elikson")
+            }
+
+            
+        }
+
     }
 }
 
